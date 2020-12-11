32905feccc73329864f9219ac572feee4826e01c
Iain Muir <iam9ez@virginia.edu>
2020-12-11 14:27:45 -0500

    Initial Commit

---
+++ fantasyClass.py
@@ -0,0 +1,22 @@
+# Iain Muir
+# iam9ez
+
+
+class FantasyLeague:
+    def __init__(self):
+        self.name = ''
+        self.members = ''
+
+
+class FantasyTeam:
+    def __init__(self):
+        self.name = ''
+        self.owner = ''
+        self.roster = ''
+
+
+class LeagueMember:
+    def __init__(self):
+        self.name = ''
+        self.team = ''
+        self.swid = ''
