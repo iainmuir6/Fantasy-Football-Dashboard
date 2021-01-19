9e8b8b336d8e2ff68b940171ed4db17d9f195bee
Iain Muir <iam9ez@virginia.edu>
2021-01-18 21:29:34 -0500

    Fantasy Class adapted for FBA and FFL

--- fantasyClass.py
+++ fantasyClass.py
@@ -3,82 +3,263 @@ Iain Muir, iam9ez
 
 Fantasy Football Classes
 
-TO-DO:
+## TODO:
     • PFF Data
     • Lock players after they've played
     • Multi-page dashboard
     • if __name__ == '__main__':
+    • constants file
 
 """
 
-import streamlit as st
 import pandas as pd
 import requests
-import pickle
-import time
 import json
-import sys
-import os
+
+# import streamlit as st
+# import pickle
+# import time
+# import sys
+# import os
 
 
 class EspnApi:
-    def __init__(self):
+    def __init__(self, sport):
         self.swid = "{94FD803F-87FC-4BDC-BD80-3F87FCEBDCFC}"
         self.espn_s2 = "AEA%2Fs7CAHybhPWiuG5cTGqmfaH2%2F%2FeUCDP5bTDB4j0hDCLGW58ieoYJCnEB9uxNNPrx1CRjc8cuCinM54m2mUnf" \
                        "5PwfVmZ6Xup0Hx1GSpuVoHuTjibfHSbu%2FDM0Zp%2BDhDygZS7zUVRZ1ag7Dm%2F7S2zcys1Ywxf4Fj1mFVKOmnIygFM" \
                        "V1a8LlMRy5W4ZpkvCvJzmQJTZcgfBncMmt1wh5IbsVWJwAm8ElISHA%2F9ooLplPppnKwIi1jbzeJv1UHIuh%2Bio%3D"
-        self.league_id = 1080747
-        self.season = 2020
-        self.week = requests.get(url='https://fantasy.espn.com/apis/v3/games/ffl/seasons/' + str(self.season) +
-                                     '/segments/0/leagues/' + str(self.league_id) + '?view=mSettings',
+        self.sport = sport
+        self.season = 2021
+
+        if sport == 'ffl':
+            self.league_id = 1080747
+            self.team_id = 4
+            self.slot_codes = {
+                0: 'QB', 1: 'TQB', 2: 'RB', 3: 'RB/WR', 4: 'WR', 5: 'WR/TE',
+                6: 'TE', 7: 'OP',  8: 'DT', 9: 'DE', 10: 'LB', 11: 'DL',
+                12: 'CB', 13: 'S', 14: 'DB', 15: 'DP', 16: 'D/ST', 17: 'K',
+                18: 'P', 19: 'HC', 20: 'BE', 21: 'IR', 22: '', 23: 'RB/WR/TE',
+                24: '', 25: 'Rookie'
+            }
+            self.default_codes = {
+                1: 'QB', 2: 'RB', 3: 'WR', 4: 'TE', 5: 'K', 16: 'D/ST'
+            }
+            self.team_ids = {
+                -16001: "atl",
+                -16002: "buf",
+                -16003: "chi",
+                -16004: "cin",
+                -16005: "cle",
+                -16006: "dal",
+                -16007: "den",
+                -16008: "det",
+                -16009: "gb",
+                -16010: "ten",
+                -16011: "ind",
+                -16012: "kc",
+                -16013: "lv",
+                -16014: "lar",
+                -16015: 'mia',
+                -16016: "min",
+                -16017: "ne",
+                -16018: "no",
+                -16019: "nyg",
+                -16020: "nyj",
+                -16021: "phi",
+                -16022: "ari",
+                -16023: "pit",
+                -16024: "lac",
+                -16025: "sf",
+                -16026: "sea",
+                -16027: "tb",
+                -16028: "wsh",
+                -16029: "car",
+                -16030: "jax",
+                -16033: 'bal',
+                -16034: 'hou',
+            }
+        elif sport == 'fba':
+            self.league_id = 1359582180
+            self.team_id = 5
+            self.slot_codes = {
+                0: 'PG',
+                1: 'SG',
+                2: 'SF',
+                3: 'PF',
+                4: 'C',
+                5: 'G',
+                6: 'F',
+                7: 'SG/SF',
+                8: 'G/F',
+                9: 'PF/C',
+                10: 'F/C',
+                11: 'UT',
+                12: 'BE',
+                13: 'IR',
+                14: '',
+                15: 'Rookie',
+                # reverse
+                'PG': 0,
+                'SG': 1,
+                'SF': 2,
+                'PF': 3,
+                'C': 4,
+                'G': 5,
+                'F': 6,
+                'SG/SF': 7,
+                'G/F': 8,
+                'PF/C': 9,
+                'F/C': 10,
+                'UT': 11,
+                'BE': 12,
+                'IR': 13,
+                'Rookie': 15
+            }
+            self.default_codes = {
+                0: 'PG',
+                1: 'SG',
+                2: 'SF',
+                3: 'PF',
+                4: 'C',
+                5: 'G',
+                6: 'F',
+                7: 'SG/SF',
+                8: 'G/F',
+                9: 'PF/C',
+                10: 'F/C',
+                11: 'UT',
+                12: 'BE',
+                13: 'IR',
+                14: '',
+                15: 'Rookie',
+                # reverse
+                'PG': 0,
+                'SG': 1,
+                'SF': 2,
+                'PF': 3,
+                'C': 4,
+                'G': 5,
+                'F': 6,
+                'SG/SF': 7,
+                'G/F': 8,
+                'PF/C': 9,
+                'F/C': 10,
+                'UT': 11,
+                'BE': 12,
+                'IR': 13,
+                'Rookie': 15
+            }
+
+            PRO_TEAM_MAP = {
+                0: 'FA',
+                1: 'ATL',
+                2: 'BOS',
+                3: 'NOP',
+                4: 'CHI',
+                5: 'CLE',
+                6: 'DAL',
+                7: 'DEN',
+                8: 'DET',
+                9: 'GSW',
+                10: 'HOU',
+                11: 'IND',
+                12: 'LAC',
+                13: 'LAL',
+                14: 'MIA',
+                15: 'MIL',
+                16: 'MIN',
+                17: 'BKN',
+                18: 'NYK',
+                19: 'ORL',
+                20: 'PHL',
+                21: 'PHO',
+                22: 'POR',
+                23: 'SAC',
+                24: 'SAS',
+                25: 'OKC',
+                26: 'UTA',
+                27: 'WAS',
+                28: 'TOR',
+                29: 'MEM',
+                30: 'CHA',
+            }
+
+            STATS_MAP = {
+                '0': 'PTS',
+                '1': 'BLK',
+                '2': 'STL',
+                '3': 'AST',
+                '4': 'OREB',
+                '5': 'DREB',
+                '6': 'REB',
+                '7': '',
+                '8': '',
+                '9': 'PF',
+                '10': '',
+                '11': 'TO',
+                '12': '',
+                '13': 'FGM',
+                '14': 'FGA',
+                '15': 'FTM',
+                '16': 'FTA',
+                '17': '3PTM',
+                '18': '3PTA',
+                '19': 'FG%',
+                '20': 'FT%',
+                '21': '3PT%',
+                '22': '',
+                '23': '',
+                '24': '',
+                '25': '',
+                '26': '',
+                '27': '',
+                '28': 'MPG',
+                '29': '',
+                '30': '',
+                '31': '',
+                '32': '',
+                '33': '',
+                '34': '',
+                '35': '',
+                '36': '',
+                '37': '',
+                '38': '',
+                '39': '',
+                '40': 'MIN',
+                '41': 'GP',
+                '42': '',
+                '43': '',
+                '44': '',
+                '45': '',
+            }
+
+            # ACTIVITY_MAP = {
+            # }
+
+            ACTIVITY_MAP = {
+                178: 'FA ADDED',
+                180: 'WAIVER ADDED',
+                179: 'DROPPED',
+                181: 'DROPPED',
+                239: 'DROPPED',
+                244: 'TRADED',
+                'FA': 178,
+                'WAIVER': 180,
+                'TRADED': 244
+            }
+
+        else:
+            print(sport)
+            exit(code=0)
+
+        self.week = requests.get(url='https://fantasy.espn.com/apis/v3/games/' + sport + '/seasons/' +
+                                     str(self.season) + '/segments/0/leagues/' + str(self.league_id) +
+                                     '?view=mSettings',
                                  cookies={
                                      "SWID": self.swid,
                                      "espn_s2": self.espn_s2
                                  }).json()['scoringPeriodId']
-        self.slot_codes = {
-            0: 'QB', 1: 'TQB', 2: 'RB', 3: 'RB/WR', 4: 'WR', 5: 'WR/TE',
-            6: 'TE', 7: 'OP',  8: 'DT', 9: 'DE', 10: 'LB', 11: 'DL',
-            12: 'CB', 13: 'S', 14: 'DB', 15: 'DP', 16: 'D/ST', 17: 'K',
-            18: 'P', 19: 'HC', 20: 'BE', 21: 'IR', 22: '', 23: 'RB/WR/TE',
-            24: '', 25: 'Rookie'
-        }
-        self.default_codes = {
-            1: 'QB', 2: 'RB', 3: 'WR', 4: 'TE', 5: 'K', 16: 'D/ST'
-        }
-        self.team_ids = {
-            -16001: "atl",
-            -16002: "buf",
-            -16003: "chi",
-            -16004: "cin",
-            -16005: "cle",
-            -16006: "dal",
-            -16007: "den",
-            -16008: "det",
-            -16009: "gb",
-            -16010: "ten",
-            -16011: "ind",
-            -16012: "kc",
-            -16013: "lv",
-            -16014: "lar",
-            -16015: 'mia',
-            -16016: "min",
-            -16017: "ne",
-            -16018: "no",
-            -16019: "nyg",
-            -16020: "nyj",
-            -16021: "phi",
-            -16022: "ari",
-            -16023: "pit",
-            -16024: "lac",
-            -16025: "sf",
-            -16026: "sea",
-            -16027: "tb",
-            -16028: "wsh",
-            -16029: "car",
-            -16030: "jax",
-            -16033: 'bal',
-            -16034: 'hou',
-        }
 
     def league_history(self):
         """
@@ -86,7 +267,8 @@ class EspnApi:
 
         :return list of league_info dictionaries for each year active
         """
-        history = requests.get(url="https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/" + str(self.league_id),
+        history = requests.get(url="https://fantasy.espn.com/apis/v3/games/" + self.sport + "/leagueHistory/" +
+                                   str(self.league_id),
                                cookies={
                                    "SWID": self.swid,
                                    "espn_s2": self.espn_s2
@@ -117,8 +299,8 @@ class EspnApi:
                              ], ...
                 }
         """
-        info = requests.get(url="https://fantasy.espn.com/apis/v3/games/ffl/seasons/" + str(self.season) +
-                                "/segments/0/leagues/" + str(self.league_id),
+        info = requests.get(url="https://fantasy.espn.com/apis/v3/games/" + self.sport + "/seasons/" +
+                                str(self.season) + "/segments/0/leagues/" + str(self.league_id),
                             cookies={
                                 "SWID": self.swid,
                                 "espn_s2": self.espn_s2
@@ -147,8 +329,9 @@ class EspnApi:
                                                'size', 'tradeSettings'])
                 }
         """
-        settings = requests.get(url='https://fantasy.espn.com/apis/v3/games/ffl/seasons/' + str(self.season) +
-                                    '/segments/0/leagues/' + str(self.league_id) + '?view=mSettings',
+        settings = requests.get(url='https://fantasy.espn.com/apis/v3/games/' + self.sport + '/seasons/' +
+                                    str(self.season) + '/segments/0/leagues/' + str(self.league_id) +
+                                    '?view=mSettings',
                                 cookies={
                                     "SWID": self.swid,
                                     "espn_s2": self.espn_s2
@@ -161,8 +344,9 @@ class EspnApi:
 
         :return
         """
-        team = requests.get(url='https://fantasy.espn.com/apis/v3/games/ffl/seasons/' + str(self.season) +
-                                '/segments/0/leagues/' + str(self.league_id) + '?view=mRoster',
+        team = requests.get(url='https://fantasy.espn.com/apis/v3/games/' + self.
+                            sport + '/seasons/' + str(self.season) + '/segments/0/leagues/' + str(self.league_id) +
+                                '?view=mRoster',
                             params={
                                 'scoringPeriodId': self.week
                             },
@@ -179,7 +363,7 @@ class EspnApi:
                 self.slot_codes[p['lineupSlotId']],
                 [round(stat['appliedTotal'], 2)
                  for stat in p['playerPoolEntry']['player']['stats']
-                 if stat['scoringPeriodId'] == self.week and stat['statSourceId'] == 1][0],
+                 if stat['scoringPeriodId'] == self.week and stat['statSourceId'] == 1],
                 [round(stat['appliedTotal'], 2)
                  for stat in p['playerPoolEntry']['player']['stats']
                  if stat['scoringPeriodId'] == self.week and stat['statSourceId'] == 0]
@@ -207,8 +391,9 @@ class EspnApi:
                                            "sortAsc": True,
                                            "value": "STANDARD"}}}
         headers = {'x-fantasy-filter': json.dumps(filters)}
-        free_agents = requests.get(url='https://fantasy.espn.com/apis/v3/games/ffl/seasons/' + str(self.season) +
-                                       '/segments/0/leagues/' + str(self.league_id) + '?view=kona_player_info',
+        free_agents = requests.get(url='https://fantasy.espn.com/apis/v3/games/' + self.sport + '/seasons/' +
+                                       str(self.season) + '/segments/0/leagues/' + str(self.league_id) +
+                                       '?view=kona_player_info',
                                    params={
                                        'scoringPeriodId': self.week
                                    },
@@ -262,75 +447,6 @@ class FantasyTeam:
         self.owners = team_info['names']
         self.roster = client.get_roster(team_id)
 
-    def dashboard(self):
-        st.markdown(
-            "<center> <img src='https://espnpressroom.com/us/files/2016/08/Fantasy-Football-App-LOGO-500x405.png' "
-            "height='200' /> </center>", unsafe_allow_html=True)
-        st.markdown("<h1 style='text-align:center';> Fantasy Lineup Optimization </h1>", unsafe_allow_html=True)
-        old, new = st.beta_columns(2)
-        old.markdown("<h3 style='text-align:center';> Current Lineup </h1>", unsafe_allow_html=True)
-        new.markdown("<h3 style='text-align:center';> Optimized Lineup </h1>", unsafe_allow_html=True)
-
-        roster_settings = {client.slot_codes[int(c)]: q for c, q in
-                           client.league_settings()['settings']['rosterSettings']['lineupSlotCounts'].items() if q != 0}
-        starters = self.roster.query('currentPosition != "BE"')
-        roster = starters.drop(axis=1, labels=['currentPosition', 'actual'])
-        free_agents = client.get_free_agents()
-        fa_names = list(free_agents['Name'])
-        options = roster.append(free_agents)
-
-        if pff:
-            pff_projections = read_ppf_data(sys.argv[1], options)
-            options = options.join(pd.DataFrame({'pffProjections': pff_projections}))
-
-        total = 0.0
-
-        for position in ['QB', 'RB', 'WR', 'TE', 'RB/WR/TE', 'D/ST', 'K']:
-            number = roster_settings[position]
-            starters_pos = starters.query('currentPosition == "' + position + '"')
-            condition = "defaultPosition == 'RB' | defaultPosition == 'WR' | defaultPosition == 'TE'" \
-                if position == 'RB/WR/TE' \
-                else "defaultPosition == '" + position + "'"
-
-            best = options.query("defaultPosition == " + condition).sort_values(by='projected',
-                                                                                ascending=False)[:number]
-            total += best['projected'].sum()
-            options = options.drop(labels=list(best.index), axis=0)
-
-            for starter, optimized in zip(starters_pos.values, best.values):
-                color1 = 'lightcoral' if starter[0] not in list(best['Name']) \
-                    else 'white'
-                color2 = 'moccasin' if optimized[0] in fa_names \
-                    else 'lightgreen' if optimized[0] not in list(starters_pos['Name']) \
-                    else 'white'
-
-                if position == 'D/ST':
-                    src1 = "https://a.espncdn.com/i/teamlogos/nfl/500/" + client.team_ids[starter[1]] + ".png"
-                    src2 = "https://a.espncdn.com/i/teamlogos/nfl/500/" + client.team_ids[optimized[1]] + ".png"
-                    height = '50'
-                else:
-                    src1 = "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/" + str(
-                        starter[1]) + ".png&w=350&h=254"
-                    src2 = "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/" + str(
-                        optimized[1]) + ".png&w=350&h=254"
-                    height = '40'
-
-                old.markdown(
-                    "<p style='text-align:left;background-color:" + color1 + ";'> <img src='" + src1 + "' height='" +
-                    height + "'/>  (" + starter[2] + ") <b>" + starter[0] + "</b>  <span style='float:right'> " +
-                    str(round(float(starter[-2]), 2)) + " </span></p>", unsafe_allow_html=True)
-                new.markdown(
-                    "<p style='text-align:left;background-color:" + color2 + ";'> <img src='" + src2 + "' height='" +
-                    height + "'/>  (" + optimized[2] + ") <b>" + optimized[0] + "</b>  <span style='float:right'> " +
-                    str(round(float(optimized[-1]), 2)) + " </span></p>", unsafe_allow_html=True)
-
-        old.markdown(
-            "<p style='text-align:center;font-size:20px;'> <b> Total: " + str(round(starters['projected'].sum(), 2)) +
-            "</b> </p>", unsafe_allow_html=True)
-        new.markdown(
-            "<p style='text-align:center;font-size:20px;'> <b> Total: " + str(round(total, 2)) +
-            "</b> </p>", unsafe_allow_html=True)
-
 
 class LeagueMember:
     def __init__(self, name, team, swid, co):
@@ -345,41 +461,5 @@ class Player:
         pass
 
 
-def read_ppf_data(data, player_options):
-    df = pd.read_csv(data)
-    projected = []
-    for name in player_options['Name']:
-        try:
-            projected.append(df.query('Name == "' + name.replace("/", "") + '"').values[0][5])
-        except IndexError:
-            projected.append(None)
-    return projected
-
-
-start = time.time()
-client = EspnApi()
-
-pff = False
-if len(sys.argv) > 1:
-    pff = True
-
-league_name = client.league_info()['settings']['name']
-if not os.path.exists("/Users/iainmuir/PycharmProjects/Desktop/espnFantasyFootball/leaguePickles/" +
-                      league_name + ".pickle"):
-    league = FantasyLeague()
-    pickle.dump(league, open("/Users/iainmuir/PycharmProjects/Desktop/espnFantasyFootball/leaguePickles/" +
-                             league_name + ".pickle", 'wb'))
-    print("League Created:", league_name)
-
-
-else:
-    league = pickle.load(open("/Users/iainmuir/PycharmProjects/Desktop/espnFantasyFootball/leaguePickles/" +
-                         league_name + ".pickle", 'rb'))
-    # league.update_teams()
-
-    league.teamObjects[4].dashboard()
-
-    pickle.dump(league, open("/Users/iainmuir/PycharmProjects/Desktop/espnFantasyFootball/leaguePickles/" +
-                             league_name + ".pickle", 'wb'))
-
-print("   --- Finished in %s seconds ---  " % round(time.time() - start, 4))
+if __name__ == '__main__':
+    client = EspnApi('ffl')
